ebc9181f4f1149df7deb68ef2b704c8151316700
Jose Luis Pintado Vasquez <i201716389@cibertec.edu.pe>
2021-09-21 13:08:00 -0500

    Base de datos

---
+++ test.sql
@@ -0,0 +1,18 @@
+INSERT INTO `claves` (`id`, `codigo`, `carrera`, `id_grado`, `id_area1`, `id_area2`, `fecha_alta`, `fecha_edit`) VALUES (NULL, '5620021', 'ANESTESIOLOGIA', '1', '2', '4', current_timestamp(), current_timestamp());
+INSERT INTO `claves` (`id`, `codigo`, `carrera`, `id_grado`, `id_area1`, `id_area2`, `fecha_alta`, `fecha_edit`) VALUES (NULL, '5620021', 'ANESTESIA', '1', '2', '4', current_timestamp(), current_timestamp());
+INSERT INTO `claves` (`id`, `codigo`, `carrera`, `id_grado`, `id_area1`, `id_area2`, `fecha_alta`, `fecha_edit`) VALUES (NULL, '5620021', 'MANTENIMIENTO DE ESTABLECIMIENTOS DE SALUD', '1', '2', '4', current_timestamp(), current_timestamp());
+INSERT INTO `claves` (`id`, `codigo`, `carrera`, `id_grado`, `id_area1`, `id_area2`, `fecha_alta`, `fecha_edit`) VALUES (NULL, '5620021', 'NUTRICIÒN Y DIETETICA', '1', '2', '4', current_timestamp(), current_timestamp());
+INSERT INTO `claves` (`id`, `codigo`, `carrera`, `id_grado`, `id_area1`, `id_area2`, `fecha_alta`, `fecha_edit`) VALUES (NULL, '5620021', 'NUTRICIÒN Y TECNOLOGÌA DE LOS ALIMENTOS', '1', '2', '4', current_timestamp(), current_timestamp());
+INSERT INTO `claves` (`id`, `codigo`, `carrera`, `id_grado`, `id_area1`, `id_area2`, `fecha_alta`, `fecha_edit`) VALUES (NULL, '5620021', 'OBSTETRICIA', '1', '2', '4', current_timestamp(), current_timestamp());
+INSERT INTO `claves` (`id`, `codigo`, `carrera`, `id_grado`, `id_area1`, `id_area2`, `fecha_alta`, `fecha_edit`) VALUES (NULL, '5620021', 'PSICOLOGIA', '1', '2', '4', current_timestamp(), current_timestamp());
+INSERT INTO `claves` (`id`, `codigo`, `carrera`, `id_grado`, `id_area1`, `id_area2`, `fecha_alta`, `fecha_edit`) VALUES (NULL, '5620021', 'FARMACIA  (6 SEMESTRES)', '1', '2', '4', current_timestamp(), current_timestamp());
+INSERT INTO `claves` (`id`, `codigo`, `carrera`, `id_grado`, `id_area1`, `id_area2`, `fecha_alta`, `fecha_edit`) VALUES (NULL, '5620021', 'LABORATORIO CLINICO', '1', '2', '4', current_timestamp(), current_timestamp());
+INSERT INTO `claves` (`id`, `codigo`, `carrera`, `id_grado`, `id_area1`, `id_area2`, `fecha_alta`, `fecha_edit`) VALUES (NULL, '5620021', 'RADIOLOGÍA', '1', '2', '4', current_timestamp(), current_timestamp());
+INSERT INTO `claves` (`id`, `codigo`, `carrera`, `id_grado`, `id_area1`, `id_area2`, `fecha_alta`, `fecha_edit`) VALUES (NULL, '5620021', 'PRIMEROS AUXILIOS', '3', '2', '4', current_timestamp(), current_timestamp());
+INSERT INTO `claves` (`id`, `codigo`, `carrera`, `id_grado`, `id_area1`, `id_area2`, `fecha_alta`, `fecha_edit`) VALUES (NULL, '5620021', 'ORTOPEDIA Y TRAUMATOLOGIA', '3', '2', '4', current_timestamp(), current_timestamp());
+INSERT INTO `claves` (`id`, `codigo`, `carrera`, `id_grado`, `id_area1`, `id_area2`, `fecha_alta`, `fecha_edit`) VALUES (NULL, '5620021', 'REFLEXOLOGIA', '3', '2', '4', current_timestamp(), current_timestamp());
+INSERT INTO `claves` (`id`, `codigo`, `carrera`, `id_grado`, `id_area1`, `id_area2`, `fecha_alta`, `fecha_edit`) VALUES (NULL, '5620021', 'ENFERMERIA TECNICA', '2', '2', '4', current_timestamp(), current_timestamp());
+INSERT INTO `claves` (`id`, `codigo`, `carrera`, `id_grado`, `id_area1`, `id_area2`, `fecha_alta`, `fecha_edit`) VALUES (NULL, '5620021', 'AUXILIAR DE ENFERMERÌA', '2', '2', '4', current_timestamp(), current_timestamp());
+INSERT INTO `claves` (`id`, `codigo`, `carrera`, `id_grado`, `id_area1`, `id_area2`, `fecha_alta`, `fecha_edit`) VALUES (NULL, '5620021', 'VOLVER A REALIZAR LA PRUEBA', '2', '2', '5', current_timestamp(), current_timestamp());
+
+
